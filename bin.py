a45d2d26e8ea63d3503496fec7c33d1354c31d4b
sergiy110888 <96384696+sergiy110888@users.noreply.github.com>
2025-02-09 15:11:29 +0300

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,12 +1,28 @@
 import numpy as np
-count=0
-##Поиск числа осуществляется с помощью Бинарного поиска
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+##    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
 def binary_search(target):
     low = 0
     high = 100
     global count
-##    Сравниваем середину числового отрезка
-##    с загаданным числом,отсекаем половину в которую число не попало
+    count=0
     while (low <= high):
         mid = (low + high) // 2
         
@@ -16,18 +32,9 @@ def binary_search(target):
         elif (mid < target):
             low = mid + 1
         else:
-            print('Число попыток: ',count)
-            return mid
-        
+            return count      
         count=count+1
-    return None
-print('Введите число X=')
-x=int(input())
-print('Загаданное число:',binary_search(x))
-        
-        
+    return None        
+score_game(binary_search)        
                 
             
-            
-        
-    
